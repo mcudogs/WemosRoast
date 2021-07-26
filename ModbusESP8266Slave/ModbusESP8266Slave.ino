360716a759e1a367e95aa266e7a64e8a3ec6b9f1
mcudogs <dsaxby53@tpg.com.au>
2021-07-26 21:11:29 +1000

    Update ModbusESP8266Slave.ino

--- ModbusESP8266Slave/ModbusESP8266Slave.ino
+++ ModbusESP8266Slave/ModbusESP8266Slave.ino
@@ -29,8 +29,7 @@ void setup()
   while (!Serial) {
  //; // wait for serial port to connect.
  }
-  Mb.begin("AP0079CF", "1462Dogs04");
-  //Mb.begin("AP0079CF", "1462Dogs04", ip, gateway, subnet);
+  Mb.begin("ssid", "password");
   delay(1000);
   Mb.MBInputRegister[0] = 0;
   Mb.MBInputRegister[1] = 0;
@@ -38,10 +37,10 @@ void setup()
   Mb.MBInputRegister[3] = 0;
   Mb.MBInputRegister[4] = 0;
 
-  Mb.MBHoldingRegister[0] = 100;
-  Mb.MBHoldingRegister[1] = 200;
-  Mb.MBHoldingRegister[2] = 300;
-  Mb.MBHoldingRegister[3] = 400;
+  Mb.MBHoldingRegister[0] = 0;
+  Mb.MBHoldingRegister[1] = 0;
+  Mb.MBHoldingRegister[2] = 0;
+  Mb.MBHoldingRegister[3] = 0;
   Mb.MBHoldingRegister[4] = 0;
   pinMode(led, OUTPUT);
   pinMode(thermoCLK, OUTPUT);
@@ -60,39 +59,15 @@ void loop()
     timer = millis();
     Mb.MBHoldingRegister[1] = ((uint16_t) thermocouple1.readCelsius()*100);
     Mb.MBHoldingRegister[0] = ((uint16_t) thermocouple.readCelsius()*100);
-    //Mb.MBHoldingRegister[0] = ((uint16_t) 20000); 
-    //Mb.MBHoldingRegister[1] = ((uint16_t) 21100);
-    //Serial.print("RSSI = ");
     Serial.println(WiFi.localIP());
-    //Serial.println("Regs = ");
-    //Serial.println(Mb.MBHoldingRegister[0]);
-    //Serial.println(Mb.MBHoldingRegister[1]);
-    //Serial.println(Mb.MBHoldingRegister[2]);
-    //Serial.println(Mb.MBHoldingRegister[3]);
-  }
+      }
 
     if(millis() - timer <= (Mb.MBHoldingRegister[2]*10))
       digitalWrite(led, HIGH);
 
     else
       digitalWrite(led, LOW);
-      
-    //delay(10);
-   //}
    
-}
-byte checkRSSI() {
-  byte quality;
-  long rssi = WiFi.RSSI();
-  if (rssi <= -100)
-    quality = 0;
-  else if (rssi >= -50)
-    quality = 100;
-  else
-    rssi = rssi + 100;
-  quality = byte(rssi * 2);
-
-  return quality;
 }
 
 
